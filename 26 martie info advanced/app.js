e3c27d968e84e898b40fbf423ed73e98882bdb3c
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-03-26 10:06:54 +0200

    Tema info 26 martie

---
+++ 26 martie info advanced/app.js
@@ -0,0 +1,12 @@
+/*ENUNT
+
+Functia golesteParagraf1() de mai jos face ca atunci cand apasam primul buton sa se goleasca primul paragraf.
+Creati o functie noua care sa goleasca al doilea paragraf atunci cand apasam al doilea buton.*/
+
+
+
+
+function golesteParagraf1() {
+
+    document.getElementById("paragraf1").innerHTML = "";
+}
