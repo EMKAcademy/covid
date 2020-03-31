2b8bfa90a2758a2585891db80fd531ef22dd40c6
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-03-31 09:59:05 +0300

    tema info 31 martie

---
+++ info - avansat 31 martie/app.js
@@ -0,0 +1,10 @@
+/*Functia legume() de mai jos schimba culoarea de fundal a listei de legume in alb si face fontul de 25px
+
+ENUNT
+Creati functia fructe() care sa schimbe culoarea de fundal a listei de legume in negru si sa faca fontul de 20px*/
+
+function legume() {
+    var legume = document.getElementById("neordonat");
+    legume.style.backgroundColor = "white";
+    legume.style.fontSize = "25px";
+}
