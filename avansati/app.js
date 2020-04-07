00e9d6cda5f50ec0456d7bf32207a923515a06ca
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-04-07 11:44:15 +0300

    Tema info 7 april

---
+++ avansati/app.js
@@ -0,0 +1,13 @@
+/*ENUNT
+
+Creati functia "formular" care sa contina:
+1. Variabila nume ce preia valoarea elementului cu id-ul "nume"
+2. Variabila prenume ce preia valoarea elementului cu id-ul "prenume"
+3. Variabile paragraf ce preia elementul cu id-ul "paragraf"
+4. Setati ca textul paragrafului sa fie "Numele complet este:" + nume + " " + prenume;  - adica sa contina numele si prenumele preluate in variabile 
+*/
+
+
+function formular() {
+  
+}
