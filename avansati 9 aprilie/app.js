f91e6b5a74dad732fc9280499f3e8db05c27e880
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-04-09 13:15:53 +0300

    Tema info avansati 9 apr

---
+++ avansati 9 aprilie/app.js
@@ -0,0 +1,11 @@
+/*ENUNT
+
+In functia culoare() declaram variabila "patrat" ce preia elementul cu id-ul "patrat", variabila 
+"dreptunghi" ce preia elementul cu id-ul "dreptunghi" si variabila "cerc" ce preia elementul
+cu id-ul "cerc". Apoi, schimbam culoarea de fundal si culoarea bordurii a fiecarei variabile.
+
+In functia schimba() declaram variabila "dreptunghi" in care preluam elementul cu id-ul
+ "dreptunghi" si variabila "dimensiune" ce preia valoarea din input. Apoi, egalam proprietatea
+ width a variabilei dreptunghi cu dimensiunea preluata. 
+
+*/
