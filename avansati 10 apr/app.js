2713472ea49233f332b64510f55f1eec2042a52b
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-04-10 10:00:42 +0300

    Tema info 10 apr

---
+++ avansati 10 apr/app.js
@@ -0,0 +1,9 @@
+/*ENUNT
+
+-Scriem functia roteste() care preia in variabila primaPoza elementul cu id-ul "primaPoza". 
+-Procedam la fel si pentru variabila aDouaPoza
+-Pentru variabila primaPoza proprietatea transform sa fie egala cu "rotate(-7deg)" 
+-Pentru variabila aDouaPoza proprietatea transform sa fie egala cu "rotate(7deg)" 
+Astfel, atunci cand dam click pe butonul "Roteste", imaginile se rotesc in partea cealalta.
+*/ 
+
