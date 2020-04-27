95bfbc852f69f1d56bdf117613d19170eb5b0d7f
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-04-27 10:03:31 +0300

    Tema info 27 aprilie

---
+++ avansati 27 apr/app.js
@@ -0,0 +1,9 @@
+/*
+- preluati in variabila "enterButton" elementul cu id-ul "enter"
+- preluati in variabila "input" elementul cu id-ul "userInput"
+- preluati in variabila "ul" elementele "ul" (folosind querySelector in loc de getElementById)
+- preluati in variabila "item" elementele "li" (folosind getElementsByTagName in loc de getElementById)
+- creati functia "inputLength()" care returneaza lungimea valorii input-ului 
+- creati functia "listLength()" care returneaza lungimea variabilei item
+*/ 
+
