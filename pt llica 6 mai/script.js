b343a5a669b5729602d035a33e2cabf20a8a3417
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-05-06 14:05:20 +0300

    Pt lica 6 mai

---
+++ pt llica 6 mai/script.js
@@ -0,0 +1,10 @@
+// Creati functia onOff care sa adauge sau sa scoata elementului cu ID-ul 'colorSelect' clasa 'onOff'
+// Creati functia white care sa faca culoarea de fundal a paginii alb si culoarea textului Negru - bonus 1p Daca faceti asta prin adaugarea clasei white 
+// Creati functia dark care sa faca culoarea de fundal a paginii negru si culoarea textului Alb - bonus 1p daca faceti asta prin adaugarea clasei dark
+                      
+
+
+
+
+
+
