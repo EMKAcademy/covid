20fd4810c99e016b7812d450470bc13f8747c3ed
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-05-14 11:35:03 +0300

    Tema 23 mai avansati

---
+++ avansati 23 mai/app.js
@@ -0,0 +1,7 @@
+/*
+- creati variabila numarImg care are initial valoarea 1;
+- creati functia "inapoi()". In ea verificam daca numarImg este mai mare decat 1. Daca da, atunci din numarImg scadem 1. 
+Tot in aceasta functie setam ca sursa elementului cu id-ul "img" sa fie "imagine" + numarImg + ".jpg". 
+- creati functia "inainte()". In ea verificam daca numarImg este mai mic decat 4. Daca da, adaugam 1 la numarImg.
+Setam din nou sursa elementului cu id-ul img, cum am facut si in cealalta functie.
+*/ 
