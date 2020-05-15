2e5724d5db861c3ba7fdb0fc20bd4bef6f106a6b
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-05-15 09:59:46 +0300

    Tema avansati 15 mai

---
+++ avansati 15 mai/app.js
@@ -0,0 +1,7 @@
+/*
+- creati functia lungimeCuvant
+- adaugam o variabila in care preluam valoarea din input
+- adaugam o variabila in care calculam numarul de litere din valoarea din input (folosind proprietatea length)
+- afisam in elementul cu id-ul "output" numarul de litere
+*/ 
+
