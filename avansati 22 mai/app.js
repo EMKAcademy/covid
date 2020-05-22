fafa8ea43734a1b62e4a7710492f10334b176be7
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2020-05-22 12:29:16 +0300

    Tema avansati 22 mai

---
+++ avansati 22 mai/app.js
@@ -0,0 +1,14 @@
+/* ENUNT
+
+Creati functia "display()" si in ea adaugati urmatoarele:
+- o variabila refresh ce are valoarea 1000;
+- setTimeout ce apeleaza functia "time()" dupa atatea milisecunde cat are refresh ca valoare.
+
+Creati functia "time()" si in ea adaugati:
+- o variabila x ce va contine data curenta (folositi new Date())
+- setam ca elementul HTML cu id-ul "ct" sa aiba ca innerText elementul x
+- apelam functia "display()"
+
+Astfel, o sa preluam o data noua o data la 1000 de milisecunde (adica o secunda) si o afisam. In final o sa para ca avem un ceas in pagina :)
+*/ 
+
